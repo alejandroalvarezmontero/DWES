d4e4397c64456e467835ddb16d2addbb807374dd
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-02-07 13:35:43 +0100

    Estructura

---
+++ 1T/Final/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const data = require('./data');
+const app = express();
+const port = process.env.PORT ||3001
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.get('/data', (req,res) =>{
+    res.send(data)
+})
+
+app.listen(port, () =>{
+    console.log(`API Rest corriendo en http://localhost:${port}`)
+})
