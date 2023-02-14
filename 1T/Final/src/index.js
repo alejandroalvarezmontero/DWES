363ac7cf4299e7e7c16b2e38b3f8a22bf9d697f9
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-02-14 20:44:04 +0100

    cambios

---
+++ 1T/Final/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require ('../routes');
+//const data = require('./data');
+
+
+const app = express();
+const port = process.env.PORT ||3001;
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Controller
+function getData(req, res) {
+    // Llamar a services (Recoger de la base de datos)
+    const data = getDataFromDatabase;
+    res.send(data);
+}
+
+// service
+function getDataFromDatabase(){
+    return {};
+}
+
