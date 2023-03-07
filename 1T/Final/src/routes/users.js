e0ccda945af169ce934e651d29c4a224b78e87b6
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-03-07 11:38:18 +0100

    Cambios

--- 1T/Final/src/routes/users.js
+++ 1T/Final/src/routes/users.js
@@ -1,11 +1,82 @@
-const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
- const { getUsers, getUserId, createUser } = require('../controllers/users.js');
+router.get('/', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('No va el servidor');
+    });
+});
 
- const router = Router();
 
- router.get('', getAllUsers);
- router.post('', createUser);
- router.get('/:id', getUserId);
+router.post('/', (req, res) => {
+  const createUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age
+  });
 
- module.exports = users;
+  createUser.save()
+    .then(user => {
+      res.json(user);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error del servidor');
+    });
+});
+
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('Este usuario no existe');
+      }
+      res.json(user);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('No va el servidor');
+    });
+});
+
+
+router.put('/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age
+  }, { new: true })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('Ese usuario no existe');
+      }
+      res.json(user);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error del servidor');
+    });
+});
+
+
+router.delete('/:id', (req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('Este usuario no existe');
+      }
+      res.sendStatus(204);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('No vale');
+    });
+});
+
+module.exports = users;
