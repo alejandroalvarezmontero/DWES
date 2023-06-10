13e2e4b5f469b5bc97e47bd8832358d240ed6f4f
alejandroalvarezmontero <95092635+alejandroalvarezmontero@users.noreply.github.com>
2023-06-10 20:58:06 +0200

    Cambio s en el back

--- Proyecto Ropa back/src/server.js
+++ Proyecto Ropa back/src/server.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const port = 3000;
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Rutas de inicio de sesión y registro
-app.post('/api/login', (req, res) => {
-  // Lógica de inicio de sesión
+app.post('/login', (req, res) => {
+ 
+  const { email, password } = req.body;
+
+  res.status(200).json({ message: 'Inicio de sesión exitoso' });
 });
 
-app.post('/api/register', (req, res) => {
-  // Lógica de registro
+app.post('/register', (req, res) => {
+  
+  const { name, email, password } = req.body;
+
+  res.status(200).json({ message: 'Usuario registrado exitosamente' });
 });
-
-// Documentación de la API con Swagger
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Carpeta pública para archivos estáticos
-app.use(express.static('public'));
 
 // Iniciar el servidor
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
+
